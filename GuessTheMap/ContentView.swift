42e2ea53a0fdf676b9e097cf535351f24538abf4
Patrick Bellot <patrickbellot77@gmail.com>
2022-05-20 14:56:11 -0500

    Initial Commit

---
+++ GuessTheMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheMap
+//
+//  Created by Patrick Bellot on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheMap/GuessTheMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheMapApp.swift
+//  GuessTheMap
+//
+//  Created by Patrick Bellot on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
